cd660ac12f7eb931b130e1feefc2667ae22d4769
Dylan <Dylan@eequ.org>
2021-09-02 21:51:45 +0100

    blog first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const articleRouter = require('./routes/articles')
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/', (req, res) => {
+  const articles = [{
+    title: 'test article',
+    createdAt: new Date(),
+    description: 'Test description'
+  },
+  {
+    title: 'test article2',
+    createdAt: new Date(),
+    description: 'Test description2'
+  }]
+  res.render('articles/index', { articles: articles })
+})
+
+app.listen(5000)
